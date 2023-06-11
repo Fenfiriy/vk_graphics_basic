c9ed04b92ab5b92ddbc7ab229074ce07b101b299
Fenfiriy <stephan.petrovvv@gmail.com>
2023-06-11 21:28:48 +0300

    HW18 initial commit

--- src/samples/shadowmap/shadowmap_render.h
+++ src/samples/shadowmap/shadowmap_render.h
@@ -47,6 +47,7 @@ private:
   etna::GlobalContext* m_context;
   etna::Image mainViewDepth;
   etna::Image shadowMap;
+  etna::Image hdrImage;
   etna::Sampler defaultSampler;
   etna::Buffer constants;
 
@@ -76,7 +77,8 @@ private:
   void* m_uboMappedMem = nullptr;
 
   etna::GraphicsPipeline m_basicForwardPipeline {};
-  etna::GraphicsPipeline m_shadowPipeline {};
+  etna::GraphicsPipeline m_shadowPipeline{};
+  etna::GraphicsPipeline m_toneMappingPipeline{};
 
   std::shared_ptr<vk_utils::DescriptorMaker> m_pBindings = nullptr;
   
